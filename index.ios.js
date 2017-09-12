cf3f72e31d7d3c23ef26913c43c46b824e6cd41c
Dang Quoc Huy <huysamdua@yahoo.com>
2017-09-12 12:49:09 +0700

    init project

---
+++ index.ios.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+'use strict';
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class PropertyFinder extends Component {
+  render() {
+    return React.createElement(Text, {style: styles.description}, "Search for houses to buy!");
+  }
+}
+
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
+  },
+});
+
+AppRegistry.registerComponent('PropertyFinder', () => PropertyFinder);
